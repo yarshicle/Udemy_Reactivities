20c7e9b22bc200d36b80999a8bde21d66370bad9
Josh Smith <joshsmithdev@gmail.com>
2026-02-03 17:42:56 -0700

    End of Section 9

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -4,6 +4,7 @@ import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetailsPage from "../../features/activities/details/ActivityDetailsPage";
+import Counter from "../../features/counter/Counter";
 
 export const router = createBrowserRouter([
 	{
@@ -14,7 +15,8 @@ export const router = createBrowserRouter([
 			{path: 'activities', element: <ActivityDashboard />},
 			{path: 'activities/:id', element: <ActivityDetailsPage />},
 			{path: 'createActivity', element: <ActivityForm key='create' />},
-			{path: 'manage/:id', element: <ActivityForm />}
+			{path: 'manage/:id', element: <ActivityForm />},
+			{path: 'counter', element: <Counter />},
 		]
 	}
 ])
